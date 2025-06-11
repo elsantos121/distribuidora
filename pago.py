13d26a110783116e155cb1345aee25beeef0f8b9
IG4141 <ivogiuliano@hotmail.com>
2025-06-11 20:13:23 -0300

    pago

---
+++ pago.py
@@ -0,0 +1,16 @@
+class Pago:
+    def seleccionar_pago(self):
+        print("\nMétodos de pago:")
+        print("1. Efectivo")
+        print("2. Tarjeta")
+        print("3. Transferencia")
+        while True:
+            opcion = input("Seleccione el método de pago: ")
+            if opcion == "1":
+                return "Efectivo"
+            elif opcion == "2":
+                return "Tarjeta"
+            elif opcion == "3":
+                return "Transferencia"
+            else:
+                print("Opción inválida.")
